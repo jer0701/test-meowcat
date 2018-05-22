ddc4d2241f7833c0102bb0bfafa6a42b16db3bf2
jer0701 <1018995917@qq.com>
2018-05-22 09:26:50 +0800

    modified

--- pages/home/home.js
+++ pages/home/home.js
@@ -34,14 +34,18 @@ Page({
     wx.getUserInfo({
       withCredentials: false,
       success: function (res) {
+        console.log(res);
         app.globalData.userInfo = res.userInfo
         that.setData({ userInfo: res.userInfo })
      
         // console.log(app.globalData.userInfo);
   
-          that.loginCheck()
+          //that.loginCheck()
 
           //that.login()
+
+          //that.updateUserInfo();
+          that.getUserInfo();
 
         
       }
@@ -50,10 +54,10 @@ Page({
 
   loginCheck: function() {
     var that = this;
-    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImQyZWU4MzViNDc2MjVjOTFiMjE1ZmVjZTU3ZGJmYTBmNDAxZThhZDciLCJpYXQiOjE1MjY0MzYyMzEsImV4cCI6MTUyNjYwOTAzMX0.sBbX3VE-kl3d7idNlG8y4EpIsV4I_0Ir7GWMNxn2FV8"
+    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImQyZWU4MzViNDc2MjVjOTFiMjE1ZmVjZTU3ZGJmYTBmNDAxZThhZDciLCJpYXQiOjE1MjY1MzgzMDYsImV4cCI6MTUyNjUzODM2Nn0.MS4AVH-xLQLlI9sfoSMxycLxP_nZgiNwJ-6zUWUZPdo";
     wx.request({
       url: 'http://localhost:7001/api/login/check',
-      header: { authorization: 'Bearer '+ token },
+      header: { authorization: token },
       method: 'GET',
       success: function (res) {
         if (res.data.status === 'ok') {
@@ -69,6 +73,7 @@ Page({
       success: function (res) {
         /** 发送登陆code给服务器*/
         console.log(res.code);
+        console.log(app.globalData.userInfo);
         wx.request({
           url: 'http://localhost:7001/api/login',
           method: 'POST',
@@ -83,7 +88,8 @@ Page({
               return
             }
             let token = res.data.token
-            wx.setStorageSync('token', token)
+            wx.setStorageSync('token', token);
+            wx.setStorageSync('openid', res.data.openid);
             console.log(wx.getStorageSync('token'))
           }, fail: function () {
             wx.showModal({
@@ -101,6 +107,63 @@ Page({
       }
     })
   },
+
+  updateUserInfo: function() {
+
+        console.log(app.globalData.userInfo);
+
+        var userInfo = app.globalData.userInfo;
+        var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImQyZWU4MzViNDc2MjVjOTFiMjE1ZmVjZTU3ZGJmYTBmNDAxZThhZDciLCJpYXQiOjE1MjY5NTIyMzIsImV4cCI6MTUyNzIxMTQzMn0.Grt8d2WquAiNBb9L9cQRb7mA2F5laZ1oX11uxvfDWrE";
+        userInfo.birthdate = new Date("1994/07/01 07:30:00");
+        wx.request({
+          url: 'http://localhost:7001/api/userinfo',
+          header: { authorization: token },
+          method: 'POST',
+          data: {
+            openid: "oxFLx5PgLr3ecvQDrV6YmghP0jG4",
+            userInfo: userInfo
+          },
+          success: function (res) {
+            console.log(res);
+            if (res.data.status !== 'ok') {
+              wx.showModal({ title: '服务器请求失败,请重试' })
+              return
+            }
+          }, fail: function () {
+            wx.showModal({
+              title: '微信出错(login)',
+              content: '本错误来自于微信本身，请尝试重启微信',
+            })
+          }
+        })
+  },
+
+  getUserInfo: function() {
+    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImQyZWU4MzViNDc2MjVjOTFiMjE1ZmVjZTU3ZGJmYTBmNDAxZThhZDciLCJpYXQiOjE1MjY5NTIyMzIsImV4cCI6MTUyNzIxMTQzMn0.Grt8d2WquAiNBb9L9cQRb7mA2F5laZ1oX11uxvfDWrE";
+    wx.request({
+      url: 'http://localhost:7001/api/userinfo',
+      method: 'GET',
+      header: { authorization: token },
+      data: {
+        openid: "oxFLx5PgLr3ecvQDrV6YmghP0jG4"
+      },
+      success: function (res) {
+        console.log(res);
+        //console.log(new Date(res.data.data.user.birthdate));
+        if (res.data.status !== 'ok') {
+          wx.showModal({ title: '服务器请求失败,请重试' })
+          return
+        }
+      }, fail: function () {
+        wx.showModal({
+          title: '微信出错(login)',
+          content: '本错误来自于微信本身，请尝试重启微信',
+        })
+      }
+    })
+},
+
+
 
   /**
    * 生命周期函数--监听页面隐藏
